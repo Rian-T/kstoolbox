4416feb203c33a058d37a905dbdb4b24115d394a
rian-t <riantouchent@gmail.com>
2020-02-09 13:45:28 +0100

    correct version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kstoolbox", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Rian Touchent",
     author_email="riantouchent@gmail.com",
     description="Small toolbox for AI Hackathon",
